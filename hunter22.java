fdc3c3cb0bda780d63d99d5d2937f84a191e6b02
manojkmkm367 <manojkmkm367@gmail.com>
2017-08-01 14:36:41 +0530

    Update hunter22.java

--- hunter22.java
+++ hunter22.java
@@ -3,16 +3,15 @@ import java.util.*;
 public class hunter22 {
 
 	public static void main(String[] args) {
-		int result=1;
+		public static void main(String[] args) {
+		int result=1,i;
 		Scanner in=new Scanner(System.in);
-		int num=in.nextInt();
+		int n=in.nextInt();
 		ArrayList<Integer> a = new ArrayList<Integer>(5);
-
-		   a.add(20);
-		   a.add(15);
-		   a.add(30);
-		   a.add(45);
-		   a.remove(num-1);
+for(i=0;i<n;i++)
+{   a.add(in.nextInt());
+}
+		   a.remove(n-1);
 		for(Integer b:a)
 		{
 			result*=b;
