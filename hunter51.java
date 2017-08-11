f8fbe03d0b109749921b265993382fc9f38cb36a
manojkmkm367 <manojkmkm367@gmail.com>
2017-08-11 11:59:38 +0530

    Create hunter51.java

---
+++ hunter51.java
@@ -0,0 +1,17 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class hunter51 {
+	public static void main(String args[])throws IOException{
+		Scanner in=new Scanner(System.in);
+		String inp1=in.next();
+		String inp2=in.next();
+		int a=Integer.parseInt(inp1);
+		int b=Integer.parseInt(inp2);
+				
+		System.out.println("OUTPUT:"+a*b);
+		
+		
+	}
+}
